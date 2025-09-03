987f05f5d79176d3abb8da96456ef8821b1f545f
Kpotapenkovs <kpootapenkovs@gmail.com>
2025-09-03 20:07:51 +0300

    veidoju JSON laiku

--- index.php
+++ index.php
@@ -1,7 +1,49 @@
-<?php
+    <?php
+
+
 $data = file_get_contents("https://emo.lv/weather-api/forecast/?city=cesis,latvia");
 
 $weatherData = json_decode($data, true);
 
-echo "city: " .$weatherData['location']['city'];
+$city = $weatherData['city']['name'];
+
+$country = $weatherData['city']['country'];
+
+$today = $weatherData['list'][0];
+
+
+$temperature = $today['temp']['day'];
+
+$minTemperature = $today['temp']['min'];
+
+$maxTemperature = $today['temp']['max'];
+
+
+
+$feelDay = $today['feels_like']['day'];
+
+$feelNight = $today['feels_like']['night'];
+
+
+$time = date('H');
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+
+?>
+</body>
+</html>
   
