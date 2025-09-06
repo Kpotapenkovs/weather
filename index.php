fc696445259571e8a025a5df99f5626dc52eca31
Kpotapenkovs <kpootapenkovs@gmail.com>
2025-09-06 20:04:14 +0300

    enable darkmode

--- index.php
+++ index.php
@@ -121,15 +121,35 @@ $time = date('H');
 
     <style>
 
+    :root{
+        --base-color: rgb(243, 244, 246 );
+        --base-variant: rgba(255, 255, 255, 1);
+        --text-color: black;
+        --secondary-text: rgba(78, 78, 78, 0);
+        --primary-color: rgba(61, 76, 83, 1);
+        --accent-color: rgba(0, 164, 240, 1);
+
+    }
+
+    .darkmode{
+        --base-color: rgb(31, 41, 55 );
+        --base-variant: rgba(52, 66, 83, 1);
+        --text-color: rgba(255, 255, 255, 0.84);
+        --secondary-text: rgba(139, 139, 139, 0.75);
+        --primary-color: rgba(133, 160, 173, 1);
+        --accent-color: rgba(0, 81, 119, 1);
+    }
+
     body {
-        background-color: rgb(243 244 246 );
+        background-color: var(--base-color);
+        color: var(--text-color);
         font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
     }
 
     div1 {
         display: flex;
-        background-color: white;
-        border: 20px solid white;
+        background-color: var(--base-variant);
+        border: 20px var(--base-variant) solid;
         width: 97%;
         height: 60px;
         box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.07);
@@ -140,8 +160,8 @@ $time = date('H');
 
         div2 {
         display: flex;
-        background-color: white;
-        border: 20px solid white;
+        background-color: var(--base-variant);
+        border: 20px solid var(--base-variant);
         width: 55%;
         height: 97%;
         box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.07);
@@ -151,8 +171,8 @@ $time = date('H');
 
         div3 {
         display: flex;
-        background-color: white;
-        border: 20px solid white;
+        background-color: var(--base-variant);
+        border: 20px solid var(--base-variant);
         width: 15%;
         height: 40%;
         box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.07);
@@ -182,20 +202,24 @@ $time = date('H');
         justify-content: left;
     }
 
+
         .container4 {
         display: flex;
         justify-content: left;
     }
 
+
     .title {
         color: rgba(0, 164, 240, 1);
     }
+
 
     .direction {
         display: flex;
         flex-direction: column;
         gap: 1px;
     }
+    
 
     * {
         margin-left: 5px;
@@ -232,11 +256,43 @@ $time = date('H');
         margin-top: 1px;
     }
 
-    .button{
-                
+    .cta-button{
+        background-color: var(--accent-color);
+    }
+
+    #theme-switch {
+        display: flex;
+        border-radius: 10px;
+        background-color: var(--base-variant);
+        justify-content: center;
+        align-items: center;
+        width: 4%;
+        height: 50%;
+        margin-left: 100px;
+    }
+
+    #theme-switch svg{
+        fill: var(--primary-color);
+
+    }
+
+    #theme-switch svg:last-child{
+        display: none;
+    }
+
+    .darkmode #theme-switch svg:first-child{
+        display: none;
+    }
+
+    .darkmode #theme-switch svg:last-child{
+        display: block;
     }
 
     </style>
+
+    <script type="text/javascript" src="darkmode.js" defer>
+    
+    </script>
 
 </head>
 <body>
@@ -249,6 +305,7 @@ $time = date('H');
         <selection class="title">
 
         <h1 class="title">VTDT Sky</h1>
+
 
 
         <form>
@@ -266,6 +323,12 @@ echo $city . ", " . $country;
 ?>
 
         </selection>
+
+        <button id="theme-switch">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#EFEFEF"><path d="M600-640 480-760l120-120 120 120-120 120Zm200 120-80-80 80-80 80 80-80 80ZM483-80q-84 0-157.5-32t-128-86.5Q143-253 111-326.5T79-484q0-146 93-257.5T409-880q-18 99 11 193.5T520-521q71 71 165.5 100T879-410q-26 144-138 237T483-80Zm0-80q88 0 163-44t118-121q-86-8-163-43.5T463-465q-61-61-97-138t-43-163q-77 43-120.5 118.5T159-484q0 135 94.5 229.5T483-160Zm-20-305Z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z"/></svg>
+            
+        </button>
 
     </div1>
 
